69b12af8c18ec84e218dd3b4ca879797b00ccd29
Saurabh Pandey <saurabhpandey8050@gmail.com>
2024-05-17 14:07:16 +0530

    17th May 2024 Lab Changes

---
+++ Project.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+html = "<!DOCTYPE html><html><head><title>Page Title</title></head><body><h3><b id='boldest'>Lebron James</b></h3><p> Salary: $ 92,000,000 </p><h3> Stephen Curry</h3><p> Salary: $85,000, 000 </p><h3> Kevin Durant </h3><p> Salary: $73,200, 000</p></body></html>"
+soup = BeautifulSoup(html, "html.parser")
+tag_object = soup.title
+print(soup.prettify())
+print("tag object:", tag_object)
