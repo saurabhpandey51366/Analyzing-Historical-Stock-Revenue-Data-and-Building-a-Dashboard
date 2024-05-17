b239c01067ff4cf674de60c948040e69c7042d0b
Saurabh Pandey <saurabhpandey8050@gmail.com>
2024-05-17 15:33:07 +0530

    Final_Assignment Library Part 1 Complete

--- Labs/Final_Assignment Library.py
+++ Labs/Final_Assignment Library.py
@@ -1,7 +1,15 @@
-s = "()[]{}"
-temp = []
-top = s[0]
-for i in s:
-    temp.append(i)
-    top = temp[0]
-    
+import yfinance as yf
+import json
+import matplotlib.pyplot as plt
+
+apple = yf.Ticker("AAPL")
+with open('D:\\Saurabh Pandey\\Softwares\\Python\\Python\\Analyzing-Historical-Stock-Revenue-Data-and-Building-a-Dashboard\\Files\\apple.json') as json_file:
+    apple_info = json.load(json_file)
+print(apple_info['country'])
+
+apple_share_price_data = apple.history(period="max")
+apple_share_price_data.head()
+apple_share_price_data.reset_index(inplace=True)
+apple_share_price_data.plot(x="Date", y="Open")
+
+print(apple.dividends)
